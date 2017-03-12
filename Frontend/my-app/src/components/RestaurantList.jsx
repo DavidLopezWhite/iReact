f0479a38c9bfc5beed25d434c452324fed90106e
Vadim Derman <thenoodly@gmail.com>
2017-03-11 18:15:42 -0800

    fixed restourant list

--- Frontend/my-app/src/components/RestaurantList.jsx
+++ Frontend/my-app/src/components/RestaurantList.jsx
@@ -3,22 +3,28 @@ import RestaurantItem from './RestaurantItem';
 import {connect} from 'react-redux';
 
 export class RestaurantList extends Component {
-  render() {
-    const {restaurantList} = this.props;
-    
-    const renderRestaurants = () => {
-      console.log(restaurantList + "hello");
-      return restaurantList.map((restaurant, index) => {
-        return (<RestaurantItem key={index} {...restaurant}/>);
-      })
-    };
+    render() {
+        const {listRestaurants} = this.props;
 
-    return (
-      <div>
-        {renderRestaurants()}
-      </div>
-    )
-  }
+        const renderRestaurants = () => {
+          return listRestaurants.map((restaurant, index) => {
+            return (<RestaurantItem key={index} {...restaurant}/>);
+          })
+        };
+
+        return (
+          <div>
+              {renderRestaurants()}
+          </div>
+        )
+    }
+
 }
 
-export default connect((state) => state)(RestaurantList);
+export default connect(
+    (state) => {
+        return {
+            listRestaurants: state.listRestaurants
+        }
+    }
+)(RestaurantList);
