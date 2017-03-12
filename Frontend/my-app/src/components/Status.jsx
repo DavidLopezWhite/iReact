1689aa225e10be4411ed2074f175f54344709023
Tan Jun Xiang <junxiang@townsquared.com>
2017-03-12 15:12:13 -0700

    Update on Status

--- Frontend/my-app/src/components/Status.jsx
+++ Frontend/my-app/src/components/Status.jsx
@@ -1,12 +1,43 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+
+const Header = styled.div`
+  border-radius: 5px;
+  background-color: gray;
+  width: 600px;
+`;
 
 export default class Status extends Component {
 
+  constructor(props) {
+    super(props);
+    this.reservation = {
+      date: 'Thursday Mar 22, 2017',
+      time: '7.30pm',
+      guests: [
+        {name: 'abc' , email: 'abc@abc.com', attending: false},
+        {name: 'bcd' , email: 'bcd@bcd.com', attending: false},
+        {name: 'cde' , email: 'cde@cde.com', attending: false}
+      ],
+      address: 'Lettuce, 2484 Marina, CA 94122',
+      attending: false,
+    }
+  }
+
   render() {
     return (
-      <div>
-          Helloworld
-      </div>
+      <Header>
+          <b>YOUR RESERVATION:</b>
+          <div>
+            <div>
+              {this.reservation.date}<br/>
+              {this.reservation.guests.length} guests at {this.reservation.time};
+            </div>
+            <div>{this.reservation.address}</div>
+          </div>
+      </Header>
+
+
     )
   }
 }
