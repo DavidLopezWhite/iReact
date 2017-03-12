8299dba8ec66f2dfeff41469f9a33c51e2e31534
Tan Jun Xiang <junxiang@townsquared.com>
2017-03-12 12:43:48 -0700

    Updated timing

--- Frontend/my-app/src/components/restaurant/Timing.jsx
+++ Frontend/my-app/src/components/restaurant/Timing.jsx
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
 
-const Time = styled.button`
+const Time = styled.a`
   display: inline-block;
+  border: 1px black solid;
+  border-radius: 5px;
+  
+  margin-bottom: 5px;
+  margin-right: 5px;
+  padding: 5px;
+
+  &:hover {
+    background-color: gray;
+    cursor: pointer;
+  }
 `;
 
 const TimeList = styled.div`
   vertical-align: top;
   display: inline-block;
-  width: 180px;
+  width: 200px;
 `;
 export default class Timing extends Component {
   onSubmit(e) {
@@ -22,7 +33,7 @@ export default class Timing extends Component {
       <TimeList>
         {
           keys.map((key) => {
-            return (<Time className={this.props.timings[key] ? 'active' : 'disabled'}>{key}</Time>)
+            return (<Time key={key} className={this.props.timings[key] ? 'active' : 'disabled'}>{key}</Time>)
           })
         }
       </TimeList>
